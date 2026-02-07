7b2de298629fda79a87bd3b2da52828c56ae6277
summer <etnsumm@gmail.com>
2026-02-07 15:55:53 +0900

    delayEdit

--- EndingA3Scene.js
+++ EndingA3Scene.js
@@ -34,7 +34,7 @@ create() {
   };
 
   // ✅ 1) 타이틀처럼 살짝 딜레이 후 자동 재생 시도
-  //this.time.delayedCall(200, startVideo);
+  this.time.delayedCall(100, startVideo);
 
   // ✅ 2) 자동재생 막히면, 유저가 한 번 터치하면 재생
   this.input.once('pointerdown', startVideo);
